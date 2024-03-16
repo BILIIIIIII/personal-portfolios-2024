17e9b3c98f7445422d188711a082396a76755bed
BILIIIIIII <ntahlah29973@gmail.com>
2024-03-16 18:33:30 +0700

    first commit 1.0

---
+++ app.js
@@ -0,0 +1,79 @@
+const projectData = [
+  {
+    link: "https://traffic-counter-mobile-programming-5.vercel.app/",
+    name: "traffic counter",
+    img: "trafficcounter.png",
+    date: { day: "07", month: " OCT", year: "2023" },
+  },
+];
+
+// {
+//     link: "https://traffic-counter-mobile-programming-5.vercel.app/",
+//     name: "TMDB Movie List",
+//     img: "trafficcounter.png",
+//     date: { day: "24", month: "JAN", year: "2O24" },
+//   },
+
+const socmed = [
+  {
+    link: "https://www.instagram.com/abillzein792/",
+    name: "instagram",
+    icon: `<i class="bx bxl-instagram"></i>`,
+  },
+  {
+    link: "https://www.linkedin.com/in/khairiabill/",
+    name: "linkedin",
+    icon: `<i class="bx bxl-linkedin-square"></i>`,
+  },
+  {
+    link: "https://github.com/BILIIIIIII/",
+    name: "github",
+    icon: `<i class="bx bxl-github"></i>`,
+  },
+];
+
+const logos = document.getElementById("logos");
+
+socmed.forEach((logo) => {
+  let card = document.createElement("div");
+
+  card.innerHTML += `
+    <a href="${logo.link}" class="bx-tada-hover" id="${logo.name}" target="_blank">
+      ${logo.icon}
+    </a>
+             
+`;
+  logos.appendChild(card);
+});
+
+const cards = document.getElementById("project-cards");
+
+projectData.forEach((data) => {
+  let card = document.createElement("div");
+  card.classList.add("card");
+
+  card.innerHTML += `
+    <a href="${data.link}" target="_blank">
+
+      <div class="info">
+        <div class="title"><p>${data.name}</p></div>
+      </div>
+
+      <div class="detail">
+
+        <div class="hero">
+          <img src="${data.img}" alt="" />
+        </div>
+
+        <div class="date">
+         ${data.date.month} <br />
+          ${data.date.year}
+        </div>
+        
+      </div>
+
+    </a>
+    `;
+
+  cards.appendChild(card);
+});
